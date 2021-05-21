ec55eb7c0a4d9604151037efeba51be465406be7
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2021-05-21 23:54:24 +0200

    shutdown pm25 sensor when not in use, to use less energy

--- pm25.py
+++ pm25.py
@@ -1,7 +1,7 @@
-import board
 from adafruit_pm25.i2c import PM25_I2C
-from digitalio import DigitalInOut
 from simpleio import map_range
+import board
+import digitalio
 import tasko
 import time
 
@@ -9,10 +9,31 @@ import time
 pm25_sensor = None
 
 
+class PM25(PM25_I2C):
+    def __init__(self, i2c_bus, set_pin=None, reset_pin=None):
+        self._reset_pin = reset_pin
+        self._i2c_bus = i2c_bus
+        self._set_pin = set_pin
+        self._set_pin.direction = digitalio.Direction.OUTPUT
+        self._set_pin.value = True
+
+        super().__init__(i2c_bus, reset_pin)
+
+    def resume(self):
+        self._set_pin.value = True
+
+    def shutdown(self):
+        self._set_pin.value = False
+
+
 def init(i2c):
     global pm25_sensor
 
-    pm25_sensor = PM25_I2C(i2c, DigitalInOut(board.D6))
+    rst = digitalio.DigitalInOut(board.D6)
+    set = digitalio.DigitalInOut(board.D5)
+
+    pm25_sensor = PM25(i2c, rst, set)
+    pm25_sensor.shutdown()
 
 
 def calculate_aqi(pm_sensor_reading):
@@ -59,16 +80,25 @@ async def sample_aq_sensor():
     Samples PM2.5 sensor
     over a 2.3 second sample rate.
     """
+    global pm25_sensor
+
     aq_reading = {"pm10": 0, "pm25": 0, "pm100": 0}
     aq_samples10 = []
     aq_samples25 = []
     aq_samples100 = []
 
+    # activate sensor
+    pm25_sensor.resume()
+    pm25_sensor = PM25(pm25_sensor._i2c_bus,
+                       pm25_sensor._reset_pin, pm25_sensor._set_pin)
+    # according to datasheet, after powering on the sensor we should
+    # wait at least 30 seconds because of fan
+    await tasko.sleep(30)
+
     # initial timestamp
     time_start = time.monotonic()
-    # when concentration is low, sensor samples every 2.3 seconds
-    # so we collect some samples and do an average
-    while time.monotonic() - time_start <= (2.3*3):
+    # gather some samples for 10 seconds and average them
+    while time.monotonic() - time_start <= 10:
         try:
             aqdata = pm25_sensor.read()
             # print("%r" % aqdata)
@@ -91,5 +121,8 @@ async def sample_aq_sensor():
     aq_samples10.clear()
     aq_samples25.clear()
     aq_samples100.clear()
+
+    # Shutdown sensor
+    pm25_sensor.shutdown()
 
     return aq_reading
